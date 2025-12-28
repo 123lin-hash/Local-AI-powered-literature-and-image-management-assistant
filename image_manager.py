7a4f1bca921128f7d6fce791ab79c9f71db0dbb9
123lin-hash <1260383529@qq.com>
2025-12-29 01:11:33 +0800

    Update image_manager.py

--- image_manager.py
+++ image_manager.py
@@ -6,22 +6,17 @@ from PIL import Image
 from sentence_transformers import SentenceTransformer
 import hashlib
 
-# ===================== 配置 =====================
-
+# 配置
 IMAGE_DIR = "./images"
 CHROMA_DIR = "./chroma_img_db"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL_PATH = "/data/linfengyun/models/sentence-transformers/clip-ViT-B-32"  # CLIP 图文模型
 
-# ===================== 加载 CLIP =====================
-
 model = SentenceTransformer(MODEL_PATH, device=device)
 
-# ===================== ChromaDB =====================
-
+#ChromaDB
 client = chromadb.PersistentClient(path=CHROMA_DIR)
-
 collection = client.get_or_create_collection(
     name="image_search",
     metadata={"hnsw:space": "cosine"}
@@ -33,7 +28,7 @@ def file_hash(path):
         h.update(f.read())
     return h.hexdigest()
 
-# ===================== 图像入库 =====================
+#图像入库
 
 def add_images(image_dir):
     for file in os.listdir(image_dir):
@@ -58,7 +53,7 @@ def add_images(image_dir):
 
         print(f"[入库] {file}")
 
-# ===================== 文本搜图 =====================
+#文本搜图
 
 def search_image(query, top_k=1):
     q_emb = model.encode(query).tolist()
@@ -73,17 +68,6 @@ def search_image(query, top_k=1):
         hits.append(meta["filename"])
 
     return hits
-
-# ===================== 主入口 =====================
-
-if __name__ == "__main__":
-
-    # 一次性入库（首次运行）
-    add_images(IMAGE_DIR)
-
-    # 文本查询
-    query = "sunset at the beach"
-    results = search_image(query)
 
     print("\n最匹配的图片：")
     for r in results:
